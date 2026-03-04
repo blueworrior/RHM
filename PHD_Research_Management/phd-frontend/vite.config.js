6b7780d1cd9eb4bf74d8ebf3127643a9347fba66
blueworrior <ssjblueworrior@gmail.com>
2026-03-05 01:29:44 +0500

    Update vite.config.js

--- PHD_Research_Management/phd-frontend/vite.config.js
+++ PHD_Research_Management/phd-frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  allowedHosts: [
+      'rhm-production-faf6.up.railway.app'
+    ]
 })
