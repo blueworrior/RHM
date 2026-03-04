f333ed87064955072f2dc87043e7329fe84a3b77
blueworrior <ssjblueworrior@gmail.com>
2026-03-05 03:29:03 +0500

    changes

--- PHD_Research_Management/phd-frontend/vite.config.js
+++ PHD_Research_Management/phd-frontend/vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      'rhm-production-ad6d.up.railway.app'
-    ]
-  },
-  preview: {
-    allowedHosts: [
-      'rhm-production-ad6d.up.railway.app'
-    ]
-  }
 })
